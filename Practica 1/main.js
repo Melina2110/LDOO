fa545c50f6050a9a2df93ae5345ff35ed1fb0989
Melina2110 <47580869+Melina2110@users.noreply.github.com>
2019-02-23 11:29:32 -0600

    Add files via upload

---
+++ Practica 1/main.js
@@ -0,0 +1,17 @@
+/* Intento fallido de java script*/
+
+
+$(document).ready(fuction(){
+		var altura = $('.menu').offset().top;
+		
+	$(window).on('scroll', fuction(){
+			if( $(window).scrollTop() > altura ){
+		$('.menu').addClass('menu-fixed');
+			} else {
+		$('.menu').removeClass('menu-fixed');
+	}
+});
+});
+
+
+
